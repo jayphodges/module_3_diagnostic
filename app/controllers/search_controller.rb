baf8f90a810c69de717a15e550d3c5de4fc6b4c1
jayphodges <jayphodges@gmail.com>
2017-10-18 10:30:33 -0600

    Experiencing issues with the API accepting the key

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     location = params[:q]
-    @conn = Faraday.new(url: "https://api.data.gov/nrel/alt-fuel-stations/") do |faraday|
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/") do |faraday|
       faraday.headers["api_key"] = ENV["ALT_FUEL_API"]
       faraday.adapter Faraday.default_adapter
     end
@@ -10,5 +10,5 @@ class SearchController < ApplicationController
 
   end
 
-
+# https://swrdfSr1SFSvXrlO3O0RpaHBE17pZBpX4HFtDQCK@api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?location=80203
 end
