da19635321da6092bcab8165a1c4867299a1b384
jayphodges <jayphodges@gmail.com>
2017-10-18 10:18:46 -0600

    Configuring faraday and inspecting the API with postman

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,14 @@
 class SearchController < ApplicationController
   def index
-    
+    location = params[:q]
+    @conn = Faraday.new(url: "https://api.data.gov/nrel/alt-fuel-stations/") do |faraday|
+      faraday.headers["api_key"] = ENV["ALT_FUEL_API"]
+      faraday.adapter Faraday.default_adapter
+    end
+    binding.pry
+    reply = @conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{location}")
+
   end
+
+
 end
